8c46b261354de6cc55e382cfd967fad4f46d9856
hisomura <hisomura.noreply@gmail.com>
2020-06-10 10:23:37 +0900

    Implement Adding fixed Item to a list.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
+import { KeyboardEventHandler, useState } from 'react'
+
 export default function Home() {
-  const todoList = ['List items', 'Add item', 'Remove todo']
+  const [todoList, setTodo] = useState(['List items', 'Add item', 'Remove todo']);
+
+  const addItem: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+    // if (event.keyCode === 229) return
+    if (event.key === 'Process') return
+    if (event.key === 'Enter') {
+      setTodo([...todoList, 'New todo'])
+    }
+    console.log(event.key)
+  }
 
   return (
     <div>
@@ -8,6 +20,7 @@ export default function Home() {
           <li>{item}</li>
         ))}
       </ul>
+      <input onKeyDown={addItem} type="text" />
     </div>
   )
 }
