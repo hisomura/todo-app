7addc51ba84bad7362b15f5bec668202aba753fd
hisomura <hisomura.noreply@gmail.com>
2020-07-18 18:43:24 +0900

    Fix sort logic in order to enable moving task to the last.

--- components/OpenTaskItem.tsx
+++ components/OpenTaskItem.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from 'react'
+import React, { MouseEvent, useState } from 'react'
 import { Task, TaskDragManager } from '../lib/task'
 
 type Props = {
@@ -11,13 +11,22 @@ type Props = {
 }
 
 export default function OpenTaskItem(props: Props) {
+  const [className, setClassName] = useState('flex py-2')
   return (
     <li
       draggable={true}
       key={props.task.id}
-      className="flex py-2"
+      className={className}
       data-testid="open-task-item"
-      onDragOver={(_e) => props.dragManager.dragOverTaskItem(props.index)}
+      onDragOver={(e) => {
+        e.preventDefault()
+        const rect = (e.target as HTMLLIElement).getBoundingClientRect()
+        const middleHeight = rect.top + rect.height / 2
+        const isUpper = middleHeight > e.clientY
+        const newClassName = 'flex py-2 ' + (isUpper ? 'border-t-2' : 'border-b-2')
+        setClassName(newClassName) // TODO currently not working
+        props.dragManager.dragOverTaskItem(isUpper ? props.index : props.index + 1)
+      }}
       onDragStart={(_e) => props.dragManager.dragStart(props.task.id, props.index)}
     >
       <input className="my-auto mr-2" type="checkbox" onClick={(e) => props.toggleTask(e, props.task)} />
