282c655ae811c36bfb8ae498d5c353aa8e9f3c97
hisomura <hisomura.noreply@gmail.com>
2020-08-01 15:30:12 +0900

    Add login button.

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,6 +1,7 @@
 import { cleanup, render, screen } from '@testing-library/react'
 import React from "react";
 import Home from "./index";
+import userEvent from "@testing-library/user-event";
 
 describe('index.tsx', () => {
   afterEach(cleanup)
@@ -8,5 +9,11 @@ describe('index.tsx', () => {
   test('shows title.', async () => {
     render(<Home />)
     expect(screen.getByText(/Todo/)).toBeVisible()
+  })
+
+  test('shows a user ID when a user click sign-in button.', async () => {
+    render(<Home />)
+    userEvent.click(screen.getByText(/Sign in/))
+    expect(await screen.findByText(/user-id-1/)).toBeVisible()
   })
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,49 @@ import React, { useEffect, useState } from 'react'
 import Todo from '../components/Todo'
 import { LocalStorageTaskRepository, TaskRepository } from '../lib/repository'
 
-export default function Home() {
-  const [repository, setRepository] = useState<TaskRepository | null>(null)
+type ApplicationState = {
+  userId: string | null
+  taskRepository: TaskRepository | null
+}
 
-  useEffect(() => setRepository(LocalStorageTaskRepository.create()), [])
-  if (!repository) {
+function useApplicationState() {
+  const [state, setState] = useState<ApplicationState>({
+    userId: null,
+    taskRepository: null,
+  })
+
+  const updateState = (params: Partial<ApplicationState>) => {
+    setState((prev) => ({ ...prev, ...params }))
+  }
+
+  return [state, updateState] as const
+}
+
+export default function Home() {
+  const [state, updateState] = useApplicationState()
+
+  useEffect(() => {
+    updateState({ taskRepository: LocalStorageTaskRepository.create() })
+  }, [])
+
+  if (!state.taskRepository) {
     return <div>now loading...</div>
   }
 
-  return <Todo repository={repository} />
+  const login = () => {
+    updateState({ userId: 'user-id-1' })
+  }
+
+  return (
+    <div>
+      {state.userId ? (
+        <div>User: {state.userId}</div>
+      ) : (
+        <button type="button" onClick={login}>
+          Sign in with Google
+        </button>
+      )}
+      <Todo repository={state.taskRepository} />
+    </div>
+  )
 }
